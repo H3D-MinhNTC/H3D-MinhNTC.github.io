5f595ccad844b447a11e00e783dfcdc098a9d792
H3D-MinhNTC <minhntc@house3d.org>
2020-08-11 18:49:52 +0700

    push 110820

--- js/resources_js.js
+++ js/resources_js.js
@@ -4,6 +4,7 @@ let isSafariNavigator = navigator.vendor && navigator.vendor.indexOf('Apple') > 
     navigator.userAgent.indexOf('CriOS') === -1 &&
     navigator.userAgent.indexOf('FxiOS') === -1;
 if (isSafari || isSafariNavigator) {
+    alert("Here");
     let doc = document.documentElement;
     let top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
     if (top <= 200) {
