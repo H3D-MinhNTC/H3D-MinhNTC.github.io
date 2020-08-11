41fa0c51760e35d01fba5fb24de1f96383e31cd2
H3D-MinhNTC <minhntc@house3d.org>
2020-08-11 19:12:46 +0700

    push 110820

--- js/resources_js.js
+++ js/resources_js.js
@@ -4,7 +4,7 @@ let isSafariNavigator = navigator.vendor && navigator.vendor.indexOf('Apple') > 
     navigator.userAgent.indexOf('CriOS') === -1 &&
     navigator.userAgent.indexOf('FxiOS') === -1;
 window.onload = function () {
-    if (isSafari || isSafariNavigator) {
+    if (!isSafari || isSafariNavigator) {
         // let doc = document.documentElement;
         // let top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
         // if (top <= 200) {
@@ -15,7 +15,7 @@ window.onload = function () {
         //     setTimeout(function () {
         //         window.scrollTo(0, top);
         //     }, 1000);
+        // }
+        document.getElementById('header').style.display = 'block';
     }
 }
-// }
-document.getElementById('header').style.display = 'block';
